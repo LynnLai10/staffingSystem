dc1225cd5fd73b2e5215a1ffc77fd1bf660a720d
LynnLai10 <52684000+LynnLai10@users.noreply.github.com>
2020-07-07 15:48:14 +1000

    fix getDate

--- client/src/components/Freetime/FreetimeNext.js
+++ client/src/components/Freetime/FreetimeNext.js
@@ -88,6 +88,7 @@ class FreetimeNext extends React.Component {
 }
 
 const mapStateToProps = ({ user }) => {
+  console.log(user.dates)
   return {
     user: user.user,
     dates: user.dates,
--- client/src/utils/getDate.js
+++ client/src/utils/getDate.js
@@ -2,20 +2,23 @@ import moment from "moment";
 
 export default (thisWeek) => {
   const schedule_No= thisWeek? `${Math.round(moment().isoWeek()/2)}` : `${Math.round(moment().isoWeek()/2)+1}`
+  //get the first day of this fortnight
   let date =
     moment().isoWeek() % 2 === 0
-      ? moment().startOf("isoWeek").add(6, "days")
-      : moment().startOf("isoWeek").add(13, "days");
-  const startDate = thisWeek? moment(date).subtract(14, "day") : moment(date).add(1, "day");
-  date = moment(startDate)
+      ? moment().startOf("isoWeek").subtract(7,"days")
+      : moment().startOf("isoWeek").add(7, "days");
+  //get the first day of the fortnight based on this schedule or next schedule
+  const startDate = thisWeek? moment(date) : moment(date).add(14, "days");
+  //get date number of the fortnight
   const dates = [];
   for (let i = 0; i < 14; i++) {
-    dates.push(date.add(1, "days").format("D"));
+    dates.push(moment(startDate).add(i, "days").format("D"));
   }
+
   return {
     dates,
     startDate: moment(startDate).format(),
-    endDate: moment(date).format(),
+    endDate: moment(startDate).add(13,'days').format(),
     schedule_No  
-  };
+  }; 
 };
